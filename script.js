481d1cd56b5c61c5a57fd71338aff7422eddb867
Ramy W. Helow <dev.ramyhelow@gmail.com>
2025-05-17 11:56:33 -0700

    Created css and javascript files with necessary components.

---
+++ script.js
@@ -0,0 +1,4 @@
+// Initialize ScrollReveal animations
+ScrollReveal().reveal('.section-title', { delay: 200 });
+ScrollReveal().reveal('.skill-card', { delay: 200, interval: 100 });
+ScrollReveal().reveal('.project-card', { delay: 200 }); 
